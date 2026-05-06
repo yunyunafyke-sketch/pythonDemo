0c9386b4b49048b8af71d31e6b8541072c37bdde
test-chenweili <yunyunafyke@gmail.com>
2026-05-06 19:00:40 +0800

    day3

--- day3/函数进阶.py
+++ day3/函数进阶.py
@@ -1,4 +1,6 @@
-#不定长参数
+#不定长参数（参数个数不确定，此时就可以使用不定长参数解决这类问题）
+#不定长位置参数，函数调用时，通过位置参数传递多个参数封装到一个元组（tuple）中
+#不定长关键字参数，函数调用时，通过关键字参数传递多个参数封装到一个字典（dict）
 def cacl_data(*args):
     min_data=min(args)
     max_data=max(args)
@@ -14,5 +16,21 @@ def cacl_data2(*args,**kwargs):
         round1= kwargs.get('digits')
     return max_data,min_data,round1
 print(cacl_data2(1,2,3,4,digits=2))
+
+
+#函数作为参数
+def add (x,y):
+    return x+y
+
+def mul (x,y,oper):
+    return oper(x,y)
+
+c=mul(1,2,add)
+print(c)
+
+
+#匿名函数
+addDemo= lambda x,y : x+y
+print(addDemo(3,4))
 
 
